6215b9e089394b0fdc3abadc20e82411fee8f4c2
musultanova <musultanova@devcats.kg>
2025-11-14 13:18:19 +0600

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Munara Mamaeva on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
